c2c14873f71ab4aae19e2fac9983fd13e3536874
Rinat Ayupov <Ayupovrinat17@yandex.ru>
2023-07-05 13:10:32 +0500

    поправил package2

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
+const helmet = require('helmet');
 const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errorHandler');
@@ -10,10 +14,13 @@ const NotFoundError = require('./errors/notFoundError');
 const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1/bitfilmsdb' } = process.env;
 
 const app = express();
+app.use(cors());
 
 mongoose.connect(MONGO_URL)
   .then(() => console.log('База данных подключена'))
   .catch((err) => console.log('Ошибка подключения к базе данных', err));
+
+app.use(helmet());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
