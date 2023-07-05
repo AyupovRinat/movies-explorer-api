ffd9e8c84c6a91f6466d145338cb64846520224a
Rinat Ayupov <Ayupovrinat17@yandex.ru>
2023-07-05 09:51:59 +0500

    добавил helmet cors dotenv

--- README.md
+++ README.md
@@ -5,7 +5,5 @@ movies-explorer-api
 
 Адрес репозитория: https://github.com/AyupovRinat/movies-explorer-api
 
-Ссылки на проект
-IP 158.160.46.197;
-
-Backend 
+IP 158.160.96.13;
+Backend https://api.ayupov.diplom.nomoreparties.sbs
--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 
 const mongoose = require('mongoose');
-
+const helmet = require('helmet');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const router = require('./routes/index');
@@ -12,10 +15,13 @@ const NotFoundError = require('./errors/notFoundError');
 const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1/bitfilmsdb' } = process.env;
 
 const app = express();
+app.use(cors());
 
 mongoose.connect(MONGO_URL)
   .then(() => console.log('База данных подключена'))
   .catch((err) => console.log('Ошибка подключения к базе данных', err));
+
+app.use(helmet());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
